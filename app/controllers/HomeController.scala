f7313380dcea1c6faa4c2f3ebc32f2e524c54a23
SinusP-CW90 <73338947+SinusP-CW90@users.noreply.github.com>
2021-10-18 22:05:11 +0200

    add working Battleship.jar

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -4,12 +4,18 @@ import javax.inject._
 import play.api._
 import play.api.mvc._
 
+import de.htwg.se.battleship.Battleship
+import de.htwg.se.battleship.controller.controllerComponent.ControllerInterface
+
+
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
 class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+  val gameController: ControllerInterface = Battleship.controller
+  def sudokuAsText =  "testi"//gameController.createEmptyBattlefield(6)
 
   /**
    * Create an Action to render an HTML page.
@@ -18,7 +24,16 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
-  def index() = Action { implicit request: Request[AnyContent] =>
+  def index(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
+
+  def about: Action[AnyContent] = Action {
+    Ok(views.html.index())
+  }
+
+  def sudoku: Action[AnyContent] = Action {
+    Ok(sudokuAsText)
+  }
+
 }
--- build.sbt
+++ build.sbt
@@ -1,5 +1,5 @@
 name := """Battleship in Scala for WebTech"""
-organization := "com.example"
+//organization := "com.example"
 
 version := "1.3"
 
@@ -10,10 +10,7 @@ resolvers += Resolver.sonatypeRepo("snapshots")
 scalaVersion := "2.13.6"
 
 libraryDependencies += guice
-
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.9" % "test"
-
-libraryDependencies += guice
 libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % Test
 
 libraryDependencies += "com.h2database" % "h2" % "1.4.200"
@@ -23,3 +20,10 @@ libraryDependencies += "com.h2database" % "h2" % "1.4.200"
 
 // Adds additional packages into conf/routes
 //play.sbt.routes.RoutesKeys.routesImport += "com.example.binders._"
+
+//libraryDependencies += "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.12.2"
+
+//dependencyOverrides += "com.fasterxml.jackson.core" % "jackson-databind" % "2.11.4"
+//dependencyOverrides += "com.fasterxml.jackson.core" % "jackson-core" % "2.11.4"
+
+//libraryDependencies += "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.13.0"
